501fe973f842a2e2c467bba735f5080116ce9c0f
santiago <santiagodeandrade4@gmail.com>
2025-02-21 08:01:53 -0400

    Creacion de entorno virtual

--- ensemble.py
+++ ensemble.py
@@ -1,11 +1,7 @@
 from utils.load_data import load_data
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.metrics import make_scorer, f1_score
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-import numpy as np
 import joblib
-from utils.precision import precision
+
 
 
 TARGET = "Class"
@@ -23,14 +19,3 @@ X_test, Y_test = [test_df.drop(TARGET, axis=1), test_df[TARGET]]
 
 print(rf_model.get_params())
 
-print("Modelo: Regresion Logistica")
-precision(Y_train, rl_model.predict(X_train), "Train")
-precision(Y_test, rl_model.predict(X_test), "Test")
-
-print("Modelo: Random Forest")
-precision(Y_train, rf_model.predict(X_train), "Train")
-precision(Y_test, rf_model.predict(X_test), "Test")
-
-print("Modelo: Naive Bayes")
-precision(Y_train, nb_model.predict(X_train), "Train")
-precision(Y_test, nb_model.predict(X_test), "Test")
